576e29d73cc252745b4d4dc933f6660911bfc369
Harsha Degala <hdegala@godaddy.com>
2023-12-03 13:24:26 +0530

    Adding Configuration changes

--- cartography/classification/run_glue.py
+++ cartography/classification/run_glue.py
@@ -71,9 +71,24 @@ except ImportError:
 
 logger = logging.getLogger(__name__)
 
+ELECTRA_PRETRAINED_CONFIG_ARCHIVE_MAP = {
+    "google/electra-small-generator": "https://huggingface.co/google/electra-small-generator/resolve/main/config.json",
+    "google/electra-base-generator": "https://huggingface.co/google/electra-base-generator/resolve/main/config.json",
+    "google/electra-large-generator": "https://huggingface.co/google/electra-large-generator/resolve/main/config.json",
+    "google/electra-small-discriminator": (
+        "https://huggingface.co/google/electra-small-discriminator/resolve/main/config.json"
+    ),
+    "google/electra-base-discriminator": (
+        "https://huggingface.co/google/electra-base-discriminator/resolve/main/config.json"
+    ),
+    "google/electra-large-discriminator": (
+        "https://huggingface.co/google/electra-large-discriminator/resolve/main/config.json"
+    ),
+}
+
 ALL_MODELS = sum(
     (
-        tuple(conf.ELECTRA_PRETRAINED_CONFIG_ARCHIVE_MAP.keys())
+        tuple(ELECTRA_PRETRAINED_CONFIG_ARCHIVE_MAP.keys())
         for conf in (
             # BertConfig,
             # RobertaConfig,
